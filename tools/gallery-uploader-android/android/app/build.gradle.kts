490b8247234cbb992680d01844cd4b343edd9f6d
ReignOfTea <reignoftea@gmail.com>
2026-07-11 00:46:32 +0100

    .

--- tools/gallery-uploader-android/android/app/build.gradle.kts
+++ tools/gallery-uploader-android/android/app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
             create("upload") {
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
             }
         }
